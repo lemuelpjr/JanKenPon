e1edacff7ca497365db7a5d8396bfb6ad8a15d02
Lemuel Pereira <lemuelpjr@gmail.com>
2019-07-02 11:21:47 -0300

    add win, lose and draw testing to the console

--- js/script.js
+++ js/script.js
@@ -1,25 +1,53 @@
 // Testing buttons for click event
-document.getElementById('rock-btn').addEventListener('click', function() {
-  // Player selected rock and the value is set to 0
-  winCheck(0);
-});
+document.getElementById('rock-btn').addEventListener('click', rockChoice);
+document.getElementById('paper-btn').addEventListener('click', paperChoice);
+document.getElementById('scissors-btn').addEventListener('click', scissorsChoice);
 
-document.getElementById('paper-btn').addEventListener('click', function() {
-  // Player selected paper and the value is set to 1
-  winCheck(1);
-});
-
-document.getElementById('scissors-btn').addEventListener('click', function() {
-  // Player selected scissors and the value is set to 2
-  winCheck(2);
-});
-
-document.getElementById('reset-btn').addEventListener('click', function () {
+document.getElementById('reset-btn').addEventListener('click', function() {
   alert('You clicked RESET!');
 });
 
-function winCheck(playerChoice) {
+// Player selected rock and the choice value is set to 0
+function rockChoice() {
   // Gets a random number between 0 and 2
   var compChoice = Math.floor(Math.random() * 3);
-  console.log(playerChoice, compChoice);
+
+  console.log(compChoice);
+  if (compChoice === 0) {
+    console.log('It is a draw!');
+  } else if (compChoice === 1) {
+    console.log('You lose!');
+  } else {
+    console.log('You win!');
+  }
+}
+
+// Player selected paper and the choice value is set to 1
+function paperChoice() {
+  // Gets a random number between 0 and 2
+  var compChoice = Math.floor(Math.random() * 3);
+
+  console.log(compChoice);
+  if (compChoice === 1) {
+    console.log('It is a draw!');
+  } else if (compChoice === 2) {
+    console.log('You lose!');
+  } else {
+    console.log('You win!');
+  }
+}
+
+// Player selected scissors and the choice value is set to 2
+function scissorsChoice() {
+  // Gets a random number between 0 and 2
+  var compChoice = Math.floor(Math.random() * 3);
+
+  console.log(compChoice);
+  if (compChoice === 2) {
+    console.log('It is a draw!');
+  } else if (compChoice === 0) {
+    console.log('You lose!');
+  } else {
+    console.log('You win!');
+  }
 }
