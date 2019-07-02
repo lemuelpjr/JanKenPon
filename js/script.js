772145d41435e6a24a53c391afe6d2b94a8f5b97
Lemuel Pereira <lemuelpjr@gmail.com>
2019-07-02 10:57:02 -0300

    add test for random number function

--- js/script.js
+++ js/script.js
@@ -1,16 +1,25 @@
 // Testing buttons for click event
 document.getElementById('rock-btn').addEventListener('click', function() {
-  alert('You clicked ROCK!');
+  // Player selected rock and the value is set to 0
+  winCheck(0);
 });
 
 document.getElementById('paper-btn').addEventListener('click', function() {
-  alert('You clicked PAPER!');
+  // Player selected paper and the value is set to 1
+  winCheck(1);
 });
 
 document.getElementById('scissors-btn').addEventListener('click', function() {
-  alert('You clicked SCISSORS!');
+  // Player selected scissors and the value is set to 2
+  winCheck(2);
 });
 
 document.getElementById('reset-btn').addEventListener('click', function () {
   alert('You clicked RESET!');
 });
+
+function winCheck(playerChoice) {
+  // Gets a random number between 0 and 2
+  var compChoice = Math.floor(Math.random() * 3);
+  console.log(playerChoice, compChoice);
+}
