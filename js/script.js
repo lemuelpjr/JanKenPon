31f2b72f9cade4ad1aa709339b369ce126bb647a
Lemuel Pereira <lemuelpjr@gmail.com>
2019-07-02 10:10:50 -0300

    test buttons for click event

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-console.log('It is working!');
+// Testing buttons for click event
+document.getElementById('rock-btn').addEventListener('click', function() {
+  alert('You clicked ROCK!');
+});
+
+document.getElementById('paper-btn').addEventListener('click', function() {
+  alert('You clicked PAPER!');
+});
+
+document.getElementById('scissors-btn').addEventListener('click', function() {
+  alert('You clicked SCISSORS!');
+});
+
+document.getElementById('reset-btn').addEventListener('click', function () {
+  alert('You clicked RESET!');
+});
